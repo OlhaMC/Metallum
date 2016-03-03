277f8108390ec83989325c690fc247a44ff00cd0
OlhaMC <AdminMC@AppleTrees-MacBook-Air.local>
2016-03-03 16:27:41 +0200

    initial commit

---
+++ Metallum/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Metallum
+//
+//  Created by AppleTree on 3/3/16.
+//  Copyright © 2016 OlhaF. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        
+        return true
+    }
+
+}
+
---
+++ Metallum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metallum
+//
+//  Created by AppleTree on 3/3/16.
+//  Copyright © 2016 OlhaF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
